ecfacb273b619f05b12154995c02d1260fefb927
IsmaelG <ismaelgn89@gmail.com>
2026-03-18 19:11:12 +0100

    exercise 2

--- index.php
+++ index.php
@@ -33,4 +33,39 @@ echo $dog1->makeSound() . "<br>" . "<br>";
 echo $cat1->makeSound() . "<br>" . "<br>";
 
 
+echo "<br>" . "-------------" . "<br>" . "<br>";
+
+
+class Shape {
+    public $width;
+    public $height;
+
+    public function __construct($width, $height) {
+        $this->width = $width;
+        $this->height = $height;
+        }
+}
+
+class Triangle extends shape {
+
+    public function area() {
+        return ($this->width * $this->height) / 2;
+    }
+}
+
+class Rectangle extends shape {
+
+    public function area() {
+        return ($this->width * $this->height);
+    
+    }
+}
+
+$triangle1 = new Triangle(2, 5);
+$rectangle1 = new Rectangle(2,4);
+
+echo "Area of triangle : " . $triangle1->area() . "<br>" . "<br>";
+echo "Area of rectangle : " . $rectangle1->area() . "<br>";
+
+
 ?>
